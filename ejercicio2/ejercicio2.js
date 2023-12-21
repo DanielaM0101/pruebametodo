90be2d11552fe945c3c7731568621b5d9de9a6fc
danny <kdmunoz@sudamericano.edu.ec>
2023-12-21 10:47:31 -0500

    Update ejercicio2.js

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -4,10 +4,10 @@
 //A a partir del array ages=[15, 50, 45] agregue su edad. Usando un for con un if, muestre en pantalla  solo las edades mayores a 25
 
 let edades = [15, 50, 45,];
-edades.splice(1,0, 21)
+edades.splice(1, 0, 21)
 for (let i = 0; i < edades.length; i++) {
-    if (edades[i]> 25)
-    alert(` ${i + 1} ${edades[i]} `)
+    if (edades[i] > 25)
+        alert(` ${i + 1} ${edades[i]} `)
 }
 
 
